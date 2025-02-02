b4438a3843d86fef8084928f7b91d9a5488cb433
vinybergamo <vinybergamo@gmail.com>
2025-02-02 02:48:15 -0300

    Adiciona decorator IsPublic aos métodos getCharge e payCharge no ChargesController

--- src/charges/charges.controller.ts
+++ src/charges/charges.controller.ts
@@ -2,11 +2,13 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ChargesService } from './charges.service';
 import { CreateChargeDto } from './dto/create-charge.dto';
 import { PayChargeDto } from './entities/pay.dto';
+import { IsPublic } from '@/helpers/decorators/is-public.decorator';
 
 @Controller('charges')
 export class ChargesController {
   constructor(private readonly chargesService: ChargesService) {}
 
+  @IsPublic()
   @Get(':chargeId')
   async getCharge(@Param('chargeId') chargeId: string) {
     return this.chargesService.findOneOrFail(chargeId);
@@ -17,6 +19,7 @@ export class ChargesController {
     return this.chargesService.create(createChargeDto);
   }
 
+  @IsPublic()
   @Post(':chargeId/pay')
   async payCharge(
     @Param('chargeId') chargeId: string,
