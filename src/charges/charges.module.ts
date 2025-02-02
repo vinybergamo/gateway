2af1cc2155618977428f9939e66fd91c55590869
vinybergamo <vinybergamo@gmail.com>
2025-02-02 16:44:41 -0300

    Atualiza a versão do pacote openpix-nestjs para 2.0.7 e ajusta a configuração do módulo Charges para usar ConfigService

--- src/charges/charges.module.ts
+++ src/charges/charges.module.ts
@@ -1,23 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ChargesService } from './charges.service';
 import { ChargesController } from './charges.controller';
-import { OpenPixModule } from 'openpix-nestjs';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Charge } from './entities/charge.entity';
 import { Customer } from '@/customers/entities/customer.entity';
 import { CustomersRepository } from '@/customers/customers.repository';
 import { ChargesRepository } from './charges.repository';
+import { OpenPixModule } from 'openpix-nestjs';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Charge, Customer]),
-    OpenPixModule.register({
-      // Stage:
-      // appId:
-      //   'Q2xpZW50X0lkXzE1MmQxNDMzLTRmODctNDcwNC05YjA0LThlN2Y0ZDc4NDVkNzpDbGllbnRfU2VjcmV0X29JYWhldWtmaThBdy9hU0pmSzlJK3ZNT21RYzhIVjhUWnlxdkx1cHhiU1k9',
-      // Production:
-      appId:
-        'Q2xpZW50X0lkXzVkNzY4Y2IwLTJjYjAtNDhhNi04MzhmLWFmZDVhN2UyMjgwMTpDbGllbnRfU2VjcmV0X2ZTZXdOU1J1TmlTUGVTUWp4aFNPMEloMGlNdHNxaWRDeUdMVTUzWDdOOWc9',
+    OpenPixModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const appId = configService.get('OPENPIX_APP_ID');
+        return {
+          appId,
+        };
+      },
     }),
   ],
   controllers: [ChargesController],
